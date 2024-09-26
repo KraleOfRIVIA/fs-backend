e983d29a5138d4b8e5a52a332a3634c99b21c434
Cahir <artur08062003@mail.ru>
2024-09-26 23:46:55 +0600

    build 1.2

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,26 @@ async function bootstrap() {
   config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  const allowedOrigins = [
+    'http://server.botoforge.ru',
+    'http://localhost:3000',
+    'http://localhost:4080',
+    'http://192.168.31.169',
+  ];
+
   // Enable CORS
   app.enableCors({
-    origin: 'http://server.botoforge.ru', // Replace with your frontend URL
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
     allowedHeaders: 'Content-Type, Authorization', // Allowed headers
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    credentials: true, // Allow credentials (cookies, etc.)
   });
 
   await app.listen(3000);
