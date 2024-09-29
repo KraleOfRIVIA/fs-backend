cc0f3293f2ce706b9cb00df67cb8fd381d042a7b
Cahir <artur08062003@mail.ru>
2024-09-29 19:53:58 +0600

    build 1.3

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,14 +8,16 @@ export class AppService {
   constructor() {
     const supabaseUrl = process.env.SUPABASE_URL;
     const supabaseKey = process.env.SUPABASE_KEY;
-    console.log(supabaseKey,supabaseUrl)
-      this.supabase = createClient(supabaseUrl, supabaseKey);
+    console.log(supabaseKey, supabaseUrl);
+    this.supabase = createClient(supabaseUrl, supabaseKey);
   }
 
   async getProducts() {
     const { data, error } = await this.supabase
       .from('ProductData')
-      .select('id, price, name, image, weight, description, category');
+      .select(
+        'id, price, name, image, weight, description, category, ingredients',
+      );
 
     if (error) {
       throw new Error(error.message);
@@ -26,11 +28,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  async getCategory(){
-    const {data, error} = await this.supabase.from('Category').select('id, name');
-    if (error){
+  async getCategory() {
+    const { data, error } = await this.supabase
+      .from('Category')
+      .select('id, name');
+    if (error) {
       throw new Error(error.message);
     }
-    return data; 
+    return data;
   }
 }
